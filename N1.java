303a291bcef085cac667ec03fb5e309c8b42cc85
unknown <movsesgrig@yahoo.com>
2015-06-27 13:45:24 +0300

    vasya

--- N1.java
+++ N1.java
@@ -1,13 +1,14 @@
 class N1 {
 	public static void main (String args[]){
 		System.out.println("a="+args[0]+" b="+args[1]);
-		int a,b,c;
+		int a,b,c,vasya;
 		a=Integer.parseInt(args[0]);
 		b=Integer.parseInt(args[1]);
 		System.out.println(a+","+b);
 		c=a;
 		a=b;
 		b=c;
+		vasya=0;
 		System.out.println(a+","+b);
 	}
 }
