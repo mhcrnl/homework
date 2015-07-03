a7d904b6e321794487c4d5ac9fe0ef00b51000cd
unknown <movsesgrig@yahoo.com>
2015-07-03 14:07:06 +0300

    Review N17

--- N17.java
+++ N17.java
@@ -1,10 +1,10 @@
 class N17{
 	public static void main(String args[]){
-		String a = String.parseString(args[0]);
+		int a = Integer.parseInt(args[0]);
 		if (a<=0){
 			System.out.println("Wrong number!");
 		}
-		else if(a%2==0 || b.length()<3 || b.length()>3){
+		else if(a%2==0 || args[0].length()<3 || args[0].length()>3){
 			System.out.println("This number IS NOT satisfying conditions of task");
 		}
 		else
